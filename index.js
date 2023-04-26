c63d4f8d0c4785cbbfcedd067588b34b12e53490
Grant Hough <grant.l.hough@gmail.com>
2023-04-26 00:44:44 -0700

    significant additions to the home page

---
+++ index.js
@@ -0,0 +1,13 @@
+$('#learn-more').on('click', function() {  
+    $('html, body').animate({
+        //first one
+        scrollTop: $("#about-container").offset().top + 50
+    }, 10);
+});
+
+$('.arrow').on('click', function() {  
+    $('html, body').animate({
+        //first one
+        scrollTop: $("#about-container").offset().top + 50
+    }, 10);
+});
